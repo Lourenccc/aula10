dcb5f7061982b1339139fe7a65cf4037c396995c
Lourenccc <167427744+Lourenccc@users.noreply.github.com>
2024-05-02 09:11:46 -0300

    Create exercicio2.c

---
+++ exercicio2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+/*
+2) Crie um algoritmo que receba 10 números e os armazene em
+um vetor A. Em seguida, gere o vetor B onde cada elemento é o
+quadrado do valor da mesma posição no vetor A
+*/
+
+int vetorA[10];
+int vetorB[10];
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+/*
+2) Crie um algoritmo que receba 10 números e os armazene em
+um vetor A. Em seguida, gere o vetor B onde cada elemento é o
+quadrado do valor da mesma posição no vetor A
+*/
+
+int vetorA[10];
+int vetorB[10];
+
+
+    for(int i=0; i > 0; i++){
+        printf("Digite um numero\n");
+        scanf("%i", &i);
+
+    }
+
+    for(int i=0; i > 5; i++){
+        vetorB[i] = vetorA[i] * vetorA[i];
+        printf("(VetorA)² = %i", vetorB[i]); 
+    }
+
+
+
+return 0; 
+}
+    for(int i=0; i > 0; i++){
+        printf("Digite um numero\n");
+        scanf("%i", &i);
+
+    }
+
+    for(int i=0; i > 5; i++){
+        vetorB[i] = vetorA[i] * vetorA[i];
+        printf("(VetorA)² = %i", vetorB[i]); 
+    }
+
+
+
+return 0; 
+}
